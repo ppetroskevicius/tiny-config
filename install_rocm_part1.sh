37333fd3cbaf79ca7014d74e58866b89c184af08
ppetroskevicius <p.petroskevicius@gmail.com>
2025-02-26 23:58:49 +0900

    feat: installs amd drivers

---
+++ install_rocm_part1.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+# https://rocm.docs.amd.com/projects/install-on-linux/en/latest/install/install-methods/package-manager/package-manager-ubuntu.html
+
+# Download and convert the package signing key
+
+# Make the directory if it doesn't exist yet.
+# This location is recommended by the distribution maintainers.
+sudo mkdir --parents --mode=0755 /etc/apt/keyrings
+
+# Download the key, convert the signing-key to a full
+# keyring required by apt and store in the keyring directory
+wget https://repo.radeon.com/rocm/rocm.gpg.key -O - \
+  | gpg --dearmor | sudo tee /etc/apt/keyrings/rocm.gpg > /dev/null
+
+# Add the AMDGPU repository for the driver
+echo "deb [arch=amd64,i386 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.3.3/ubuntu noble main" \
+  | sudo tee /etc/apt/sources.list.d/amdgpu.list
+sudo apt update
+
+# Add the ROCm repository
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/6.3.3 noble main" \
+  | sudo tee --append /etc/apt/sources.list.d/rocm.list
+echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+  | sudo tee /etc/apt/preferences.d/rocm-pin-600
+sudo apt update
+
+# Install kernel driver
+sudo apt install amdgpu-dkms
+sudo reboot
---
+++ install_rocm_part2.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -euo pipefail
+set -x
+
+# Install ROCm packages
+sudo apt install rocm
+
+# Post install steps:
+
+# Configure the system linker by indicating where to find the shared objects (.so files) for the ROCm applications
+sudo tee --append /etc/ld.so.conf.d/rocm.conf << EOF
+/opt/rocm/lib
+/opt/rocm/lib64
+EOF
+sudo ldconfig
+
+# Configure the path to the ROCm binary
+update-alternatives --list rocm
+
+# Verify the kernel-mode driver installation
+dkms status
+
+# Verify the ROCm installation
+sudo rocminfo
+clinfo
+
+# Show available memory
+rocm-smi --showmeminfo vram
+
+# Verify the package installation
+apt list --installed
+
+# Monitor GPU
+watch -n 1 rocm-smi
+
+sudo apt install rocm-bandwidth-test
+# rocm-bandwidth-test
