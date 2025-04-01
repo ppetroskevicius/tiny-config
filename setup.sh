cf2480030f34df31d2a4c1564a56a119fdbab6a7
ppetroskevicius <p.petroskevicius@gmail.com>
2025-04-01 23:15:21 +0900

    feat: add docker install

--- setup.sh
+++ setup.sh
@@ -421,6 +421,25 @@ install_llvm_mlir() {
   sudo apt install -y clang llvm lldb lld
 }
 
+install_docker() {
+  # Add Docker's official GPG key:
+  sudo apt-get update
+  sudo apt-get install ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  # Add the repository to Apt sources:
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" \
+    | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt-get update
+
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo docker run hello-world
+}
+
 install_1password_app() {
   sudo apt install -y 1password
 }
