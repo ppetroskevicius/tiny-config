b4c7668e6336675cfc3367ebb7919e6a10d9f163
ppetroskevicius <p.petroskevicius@gmail.com>
2025-04-13 20:48:52 +0900

    fix: aws cli installw

--- install_common_dependencies.sh
+++ install_common_dependencies.sh
@@ -125,10 +125,19 @@ install_node() {
 
 install_aws_cli() {
   if ! command -v aws > /dev/null; then
-    curl "https://awscli.amazonaws.com/awscli-exe-${OS,,}-${ARCH}.zip" -o awscliv2.zip
-    unzip awscliv2.zip
-    sudo ./aws/install
-    rm -rf awscliv2.zip aws
+    if [ "$OS" = "Darwin" ]; then
+      curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "awscliv2.pkg"
+      sudo installer -pkg awscliv2.pkg -target /
+      rm -f awscliv2.pkg
+    else
+      curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+      unzip awscliv2.zip
+      sudo ./aws/install
+      rm -rf awscliv2.zip aws
+    fi
+    aws --version
+
+    # Install AWS CDK
     sudo npm install -g aws-cdk
     cdk --version
   fi
